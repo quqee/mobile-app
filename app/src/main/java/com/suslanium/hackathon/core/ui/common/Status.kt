df142e425a81efe3d0628621b0fc9e9fbf261b7a
Alexander <73055263+cah999@users.noreply.github.com>
2024-03-28 16:00:58 +0500

    added ui for defect screen

--- app/src/main/java/com/suslanium/hackathon/core/ui/common/Status.kt
+++ app/src/main/java/com/suslanium/hackathon/core/ui/common/Status.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,61 +25,52 @@ import com.suslanium.hackathon.core.ui.theme.S12_W400
 import com.suslanium.hackathon.core.ui.theme.Yellow
 
 @Composable
-fun StatusElement(status: Status) {
-    when (status) {
-        Status.DONE -> {
-            Row(verticalAlignment = Alignment.CenterVertically) {
+fun StatusElement(modifier: Modifier = Modifier, status: Status) {
+    Row(verticalAlignment = Alignment.CenterVertically, modifier = modifier) {
+        when (status) {
+            Status.DONE -> {
                 Icon(imageVector = Icons.Default.Done, contentDescription = null, tint = Green)
-                Spacer(modifier = androidx.compose.ui.Modifier.width(8.dp))
+                Spacer(modifier = Modifier.width(8.dp))
                 Text(text = stringResource(R.string.fixed), color = Green)
             }
-        }
 
-        Status.IN_PROGRESS -> {
-            Row(verticalAlignment = Alignment.CenterVertically) {
+            Status.IN_PROGRESS -> {
                 Icon(imageVector = Icons.Default.Timeline, contentDescription = null, tint = Yellow)
-                Spacer(modifier = androidx.compose.ui.Modifier.width(8.dp))
+                Spacer(modifier = Modifier.width(8.dp))
                 Text(text = stringResource(R.string.in_progress), color = Yellow)
             }
-        }
 
-        Status.WAITING -> {
-            Row(verticalAlignment = Alignment.CenterVertically) {
+            Status.WAITING -> {
                 Icon(
                     imageVector = Icons.Default.IncompleteCircle,
                     contentDescription = null,
                     tint = Primary
                 )
-                Spacer(modifier = androidx.compose.ui.Modifier.width(8.dp))
+                Spacer(modifier = Modifier.width(8.dp))
                 Text(text = stringResource(R.string.waiting), color = Primary)
             }
-        }
 
-        Status.CANCELLED -> {
-            Row(verticalAlignment = Alignment.CenterVertically) {
+            Status.CANCELLED -> {
+
                 Icon(imageVector = Icons.Default.Cancel, contentDescription = null, tint = Error)
-                Spacer(modifier = androidx.compose.ui.Modifier.width(8.dp))
+                Spacer(modifier = Modifier.width(8.dp))
                 Text(text = stringResource(R.string.cancelled), style = S12_W400, color = Error)
             }
         }
     }
-
 }
 
 enum class Status {
-    DONE,
-    IN_PROGRESS,
-    CANCELLED,
-    WAITING,
+    DONE, IN_PROGRESS, CANCELLED, WAITING,
 }
 
 @Preview
 @Composable
 private fun StatusPreview() {
     Column {
-        StatusElement(Status.DONE)
-        StatusElement(Status.IN_PROGRESS)
-        StatusElement(Status.WAITING)
-        StatusElement(Status.CANCELLED)
+        StatusElement(status = Status.DONE)
+        StatusElement(status = Status.WAITING)
+        StatusElement(status = Status.IN_PROGRESS)
+        StatusElement(status = Status.CANCELLED)
     }
 }
