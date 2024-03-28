34e6d302902f507b53897e8c9182235771705cd5
lantt <lanttoff@gmail.com>
2024-03-28 11:00:46 +0500

    add onboarding screen

---
+++ app/src/main/java/com/suslanium/hackathon/onboarding/presentation/OnboardingScreen.kt
@@ -0,0 +1,78 @@
+package com.suslanium.hackathon.onboarding.presentation
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.suslanium.hackathon.R
+import com.suslanium.hackathon.core.ui.common.PrimaryButton
+import com.suslanium.hackathon.core.ui.theme.DarkBlue
+import com.suslanium.hackathon.core.ui.theme.PaddingLarge
+import com.suslanium.hackathon.core.ui.theme.PaddingMedium
+import com.suslanium.hackathon.core.ui.theme.PaddingXLarge
+import com.suslanium.hackathon.core.ui.theme.PaddingXXLarge
+import com.suslanium.hackathon.core.ui.theme.S15_W400
+import com.suslanium.hackathon.core.ui.theme.S20_W700
+
+@Composable
+fun OnboardingScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(horizontal = PaddingMedium),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.weight(0.7f))
+
+        Image(
+            imageVector = ImageVector.vectorResource(id = R.drawable.logo),
+            contentDescription = null
+        )
+
+        Spacer(modifier = Modifier.height(PaddingXLarge))
+
+        Text(
+            text = stringResource(id = R.string.appName),
+            style = S20_W700,
+            color = DarkBlue,
+        )
+
+        Spacer(modifier = Modifier.height(PaddingLarge))
+
+        Text(
+            text = stringResource(id = R.string.startDescription),
+            style = S15_W400,
+            color = DarkBlue,
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        PrimaryButton(
+            modifier = Modifier.padding(bottom = PaddingXXLarge),
+            text = stringResource(id = R.string.logIn),
+            onClick = {
+                // TODO navigate to auth
+            }
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun Prev() {
+    OnboardingScreen()
+}
