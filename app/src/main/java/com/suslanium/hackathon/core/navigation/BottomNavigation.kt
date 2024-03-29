7f6bd3d29d8597edf00b4da848008e3f68551fa5
Suslanium <zaharov22m@gmail.com>
2024-03-29 15:08:21 +0700

    Map placeholder

--- app/src/main/java/com/suslanium/hackathon/core/navigation/BottomNavigation.kt
+++ app/src/main/java/com/suslanium/hackathon/core/navigation/BottomNavigation.kt
@@ -1,10 +1,15 @@
 package com.suslanium.hackathon.core.navigation
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.suslanium.hackathon.defect.presentation.ui.components.MapView
 import com.suslanium.hackathon.statements.presentation.screen.StatementsScreen
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.CameraPosition
 
 @Composable
 fun BottomNavigation(
@@ -26,11 +31,10 @@ fun BottomNavigation(
             )
         }
         composable(RoadCareDestinations.MAP) {
-            PlaceholderScreen(
-                text = "map",
-                onButtonClick = {},
-                buttonText = "placeholder"
-            )
+            MapView(startCameraPosition = CameraPosition(
+                Point(57.993764, 56.216534),
+                17.0f, 150.0f, 30.0f
+            ), modifier = Modifier.fillMaxSize(), blockInput = false)
         }
         composable(RoadCareDestinations.PROFILE) {
             PlaceholderScreen(
